03efe7479329923cda2b89807be49064ebd651a1
Hubert <19597269+HKGx@users.noreply.github.com>
2022-11-15 22:07:18 +0100

    Downgrade typings used in __init__.py

--- perhaps/__init__.py
+++ perhaps/__init__.py
@@ -1,8 +1,10 @@
 __version__ = "0.1.0"
 
+from typing import Tuple
+
 from .maybe import Just, Maybe, Nothing
 
-__all__: tuple[str, ...] = (
+__all__: Tuple[str, ...] = (
     "Maybe",
     "Just",
     "Nothing",
