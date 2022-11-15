0d8fada36217a9fc557e181901ec96e80fef056c
Hubert <19597269+HKGx@users.noreply.github.com>
2022-11-15 21:10:11 +0100

    first commit

---
+++ perhaps/maybe.py
@@ -0,0 +1,155 @@
+from abc import ABC, abstractmethod
+from typing import Callable, Generic, Never, NoReturn, Type, TypeVar, overload
+
+T = TypeVar("T")
+R = TypeVar("R")
+
+
+class Maybe(Generic[T], ABC):
+    value: T | Never
+
+    @abstractmethod
+    def map(self, f: Callable[[T], R]) -> "Maybe[R]":
+        ...
+
+    @abstractmethod
+    def or_else(self, f: Callable[[], T]) -> T:
+        ...
+
+    @abstractmethod
+    def unwrap(
+        self, f: Callable[[], Exception | Type[Exception]] | None = None
+    ) -> T | NoReturn:
+        ...
+
+    @abstractmethod
+    def to_optional(self) -> T | None:
+        ...
+
+    @classmethod
+    def from_optional(cls, value: T | None) -> "Maybe[T]":
+        return Just(value) if value is not None else Nothing()
+
+    @classmethod
+    def from_try(
+        cls, f: Callable[[], T], exc: Type[Exception] | tuple[Type[Exception], ...] = ()
+    ) -> "Maybe[T]":
+        try:
+            return Just(f())
+        except exc:
+            return Nothing()
+
+    @abstractmethod
+    def __and__(self, other: "Maybe[R]") -> "Maybe[T] | Maybe[R]":
+        ...
+
+    @abstractmethod
+    def __or__(self, other: "Maybe[R]") -> "Maybe[T] | Maybe[R]":
+        ...
+
+    @abstractmethod
+    def __eq__(self, other: object) -> bool:
+        ...
+
+    @abstractmethod
+    def __repr__(self) -> str:
+        ...
+
+
+class Just(Generic[T], Maybe[T]):
+    __match_args__ = ("value",)
+
+    value: T
+
+    def __init__(self, value: T):
+        self.value = value
+
+    def map(self, f: Callable[[T], R]) -> "Just[R]":
+        return Just(f(self.value))
+
+    def or_else(self, f: Callable[[], T]) -> T:
+        return self.value
+
+    def unwrap(self, f: Callable[[], Exception | Type[Exception]] | None = None) -> T:
+        return self.value
+
+    def to_optional(self) -> T:
+        return self.value
+
+    @overload
+    def __and__(self, other: "Just[R]") -> "Just[R]":
+        ...
+
+    @overload
+    def __and__(self, other: "Nothing[R]") -> "Nothing[R]":
+        ...
+
+    def __and__(self, other: "Maybe[R]") -> "Maybe[T] | Maybe[R]":
+        return other
+
+    @overload
+    def __or__(self, other: "Just") -> "Just[T]":
+        ...
+
+    @overload
+    def __or__(self, other: "Nothing") -> "Just[T]":
+        ...
+
+    def __or__(self, other: "Maybe[R]") -> "Maybe[T] | Maybe[R]":
+        return self
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, Just) and self.value == other.value
+
+    def __repr__(self) -> str:
+        return f"Just({self.value!r})"
+
+
+class Nothing(Generic[T], Maybe[T]):
+    __match_args__ = ()
+
+    def map(self, f: Callable[[T], R]) -> "Nothing[R]":
+        return Nothing()
+
+    def or_else(self, f: Callable[[], T]) -> T:
+        return f()
+
+    def unwrap(
+        self, f: Callable[[], Exception | Type[Exception]] | None = None
+    ) -> NoReturn:
+        if f:
+            raise f()
+        raise ValueError("Tried to unwrap Nothing")
+
+    def to_optional(self) -> T | None:
+        return None
+
+    @overload
+    def __and__(self, other: "Just") -> "Nothing[T]":
+        ...
+
+    @overload
+    def __and__(self, other: "Nothing") -> "Nothing[T]":
+        ...
+
+    def __and__(self, other: "Maybe[R]") -> "Maybe[T] | Maybe[R]":
+        return self
+
+    @overload
+    def __or__(self, other: "Just[R]") -> "Just[R]":
+        ...
+
+    @overload
+    def __or__(self, other: "Nothing[R]") -> "Nothing[R]":
+        ...
+
+    def __or__(self, other: "Maybe[R]") -> "Nothing[T] | Maybe[R]":
+        return other
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, Nothing)
+
+    def __repr__(self) -> str:
+        return "Nothing()"
+
+
