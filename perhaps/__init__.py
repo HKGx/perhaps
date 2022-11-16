e78c9445e9655325da65c288a789c4eb27030db0
Hubert <19597269+HKGx@users.noreply.github.com>
2022-11-16 23:14:07 +0100

    Improve the way we handle __version__

--- perhaps/__init__.py
+++ perhaps/__init__.py
@@ -1,8 +1,9 @@
-__version__ = "0.3.2"
-
+from importlib.metadata import version
 from typing import Tuple
 
 from .maybe import Just, Maybe, Nothing
+
+__version__ = version("perhaps")
 
 __all__: Tuple[str, ...] = (
     "Maybe",
